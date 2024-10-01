7ddaba48beb1679eefa7ded6b591311fe07b6c5a
Milton <milton761@gmail.com>
2024-10-01 13:32:54 -0500

    remove unused code in TWApplication

--- app/src/main/java/com/tws/moments/TWApplication.kt
+++ app/src/main/java/com/tws/moments/TWApplication.kt
@@ -1,29 +1,7 @@
 package com.tws.moments
 
 import android.app.Application
-import com.tws.moments.imageloader.GlideImageLoader
-import com.tws.moments.imageloader.ImageLoader
-import com.tws.moments.utils.ScreenAdaptiveUtil
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TWApplication : Application() {
-
-//    // TODO avoid memory leak
-//    companion object {
-//        lateinit var app: Application
-//        lateinit var imageLoader: ImageLoader
-//    }
-
-    override fun onCreate() {
-        super.onCreate()
-//        app = this
-//        ScreenAdaptiveUtil.adaptive(this)
-
-//        initImageLoader()
-    }
-
-//    private fun initImageLoader() {
-//        imageLoader = GlideImageLoader(this)
-//    }
-}
+class TWApplication : Application()
